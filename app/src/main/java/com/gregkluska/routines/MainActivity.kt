f330a5798a127c20b7c2687d1531e8a50a01f10c
Greg Kluska <kluskagreg@gmail.com>
2020-11-24 20:55:44 +0000

    Initial commit

---
+++ app/src/main/java/com/gregkluska/routines/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.gregkluska.routines
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
+import com.gregkluska.routines.ui.RoutinesTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RoutinesTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    RoutinesTheme {
+        Greeting("Android")
+    }
+}
